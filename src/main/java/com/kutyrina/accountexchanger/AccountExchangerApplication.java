de3cde87ce34bccac9f235bc34b56b5723c27726
Darya <foxik_dary@mail.ru>
2021-09-23 17:17:45 +0300

    initial

---
+++ src/main/java/com/kutyrina/accountexchanger/AccountExchangerApplication.java
@@ -0,0 +1,13 @@
+package com.kutyrina.accountexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountExchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountExchangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kutyrina/accountexchanger/AccountExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutyrina.accountexchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountExchangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
