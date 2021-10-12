d463aee36559343046a21240b3f7dd42469d60db
Darya <foxik_dary@mail.ru>
2021-10-12 14:51:18 +0300

    modified test for transferMoneyToAnotherUser.

--- src/test/java/com/kutyrina/accountexchanger/account/service/AccountServiceTest.java
+++ src/test/java/com/kutyrina/accountexchanger/account/service/AccountServiceTest.java
@@ -73,13 +73,13 @@ public class AccountServiceTest {
         accountFrom.setAccountNumber(accountFromId);
         accountFrom.setBalance(new BigDecimal("100"));
 
-        doReturn(Optional.of(accountFrom)).when(accountRepository).findById(accountFromId);
+        doReturn(Optional.of(accountFrom)).when(accountRepository).findByAccountNumberWithLock(accountFromId);
 
         Account accountTo = new Account();
         accountTo.setAccountNumber(accountToId);
         accountTo.setBalance(new BigDecimal("50"));
 
-        doReturn(Optional.of(accountTo)).when(accountRepository).findById(accountToId);
+        doReturn(Optional.of(accountTo)).when(accountRepository).findByAccountNumberWithLock(accountToId);
 
         TransferMoneyRequest transferMoneyRequest = new TransferMoneyRequest();
         transferMoneyRequest.setAccountFrom(accountFromId);
@@ -88,8 +88,8 @@ public class AccountServiceTest {
 
         accountServiceMock.transferMoneyToAnotherUser(transferMoneyRequest);
 
-        verify(accountRepository).findById(accountFromId);
-        verify(accountRepository).findById(accountToId);
+        verify(accountRepository).findByAccountNumberWithLock(accountFromId);
+        verify(accountRepository).findByAccountNumberWithLock(accountToId);
         verify(accountRepository, times(2)).save(any());
     }
 }
